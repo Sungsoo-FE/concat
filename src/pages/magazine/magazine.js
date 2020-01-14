ba7900101f8b553a592884f60292a4195b6155ed
kwtjdtn <kwtjdtn1@naver.com>
2020-01-14 15:41:04 +0900

    center align magazine page

--- src/pages/magazine/magazine.js
+++ src/pages/magazine/magazine.js
@@ -18,9 +18,12 @@ const Magazine = () => {
             <div className = "mag_logo">
                 <img className="mag_logo_img" src= { mag_logo } alt = "mag_logo"/>    
             </div>
-            <div className = "main-wrapper">
-                <h2 className = "main-text">당신을 위한 매거진 <span>🗒</span></h2>
-                <input className="searchbar" name="keyword" placeholder = "매거진 검색하기"/>  
+            <div className = "main-wrap-wrapper">
+                <div className = "main-wrapper">
+                    <h2 className = "main-text">당신을 위한 매거진 🗒</h2>
+                    <div className = "txt-wrapper"></div>
+                    <input className="searchbar" name="keyword" placeholder = "매거진 검색하기"/>  
+                </div>
             </div>
             <div className = "mag-wrapper">
                 <div className= "maga">
