6bffaee47698a3c268881a7e8b98bd46baf08e72
kwtjdtn <kwtjdtn1@naver.com>
2020-01-29 16:52:56 +0900

    content에news 링크 걸기

--- src/pages/magazine/magazine.js
+++ src/pages/magazine/magazine.js
@@ -27,45 +27,45 @@ const Magazine = () => {
             <div className = "mag-wrapper">
                 <div className= "maga">
                     <div className = "divChild">
-                        <a className = "cate-box">보험</a>
-                        <Link to = "/content" ><img src = { mag_img1 } className = "m-img" alt = "mag_img1"></img></Link>
+                        <a className = "cate-box">해킹</a>
+                        <a href = 'http://now.rememberapp.co.kr/2020/01/29/6536/' ><img src = { mag_img1 } className = "m-img" alt = "mag_img1"></img></a>
                     </div>
                     <div className = "divChild">
-                        <a className = "cate-box2">보험</a>
-                        <Link to = "/content"><img src = { mag_img2} className = "m-img2" alt = "mag_img2"></img></Link> 
+                        <a className = "cate-box2">콘텐츠</a>
+                        <a href = 'http://now.rememberapp.co.kr/2020/01/28/6517/'><img src = { mag_img2} className = "m-img2" alt = "mag_img2"></img></a> 
                     </div>
                 </div>
                 <div/>
                 <div className = "maga">
                     <div className= "divChild">
-                        <a className = "cate-box">주식</a>
-                        <Link to = "/content" ><img src = { mag_img3 } className = "m-img" alt = "mag_img3"></img></Link> 
+                        <a className = "cate-box">갭투자</a>
+                        <a href = 'http://now.rememberapp.co.kr/2020/01/28/6508/' ><img src = { mag_img3 } className = "m-img" alt = "mag_img3"></img></a> 
                     </div>
                     <div className= "divChild">
+                        <a className = "cate-box2">경제성장</a>
+                        <a href = 'http://now.rememberapp.co.kr/2020/01/23/6501/'><img src = { mag_img4 } className = "m-img2" alt = "mag_img4"></img></a> 
+                    </div>
+                </div>
+                <div/>
+                <div className = "maga">
+                    <div className = "divChild">
+                        <a className = "cate-box">재테크</a>
+                        <a href = 'http://now.rememberapp.co.kr/2020/01/22/6479/'><img src = { mag_img5 } className = "m-img" alt = "mag_img5"></img></a> 
+                    </div>
+                    <div className = "divChild">
                         <a className = "cate-box2">가상화폐</a>
-                        <Link to = "/content"><img src = { mag_img4 } className = "m-img2" alt = "mag_img4"></img></Link> 
+                        <a href = "/content"><img src = { mag_img6 } className = "m-img2" alt = "mag_img6"></img></a> 
                     </div>
                 </div>
                 <div/>
                 <div className = "maga">
                     <div className = "divChild">
                         <a className = "cate-box">가상화폐</a>
-                        <Link to = "/content"><img src = { mag_img5 } className = "m-img" alt = "mag_img5"></img></Link> 
+                        <a href ="/content"><img src = { mag_img7 } className = "m-img" alt = "mag_img7"></img></a> 
                     </div>
                     <div className = "divChild">
                         <a className = "cate-box2">가상화폐</a>
-                        <Link to = "/content"><img src = { mag_img6 } className = "m-img2" alt = "mag_img6"></img></Link> 
-                    </div>
-                </div>
-                <div/>
-                <div className = "maga">
-                    <div className = "divChild">
-                        <a className = "cate-box">가상화폐</a>
-                        <Link to ="/content"><img src = { mag_img7 } className = "m-img" alt = "mag_img7"></img></Link> 
-                    </div>
-                    <div className = "divChild">
-                        <a className = "cate-box2">가상화폐</a>
-                        <Link to ="/content"><img src = { mag_img8 } className = "m-img2" alt = "mag_img8"></img></Link> 
+                        <a href ="/content"><img src = { mag_img8 } className = "m-img2" alt = "mag_img8"></img></a> 
                     </div>
                 </div>
                 <div/>
